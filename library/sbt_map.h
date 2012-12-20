12d49c15b4f9d5a87ee19c8ebfe1b2cdf899dc81
sd <sd542927172@gmail.com>
2012-12-20 21:35:36 +0800

    2012/12/20
    第一次上传git

--- library/sbt_map.h
+++ library/sbt_map.h
@@ -3,13 +3,28 @@ using namespace std;
 
 template <typename T_key,typename T_val>
 class map{
+public:
+		typedef T_key		key_type;
+		typedef T_val		val_type;
+		typedef _Compare	key_compare;
 private:
+		struct Node{
+				T_key key;
+				T_val val;
+				long size;
+				Node *lf, *rt;
+		};
 		long size;
+		Node * root;
+		Node * insert(Node*,T_key,T_val);
+		void erase(T_key);
+		void release(Node & * root);
+		map();
 public:
 		void clear();
-		T_val findbykey(T_key);
-		T_val operator[](T_key now){
-				return findbykey(now);
+		T_val& at(T_key);		
+template <typename T_key,typename T_val>
+		T_val& operator[](T_key now){
+				return at(now);
 		}
-
 };
